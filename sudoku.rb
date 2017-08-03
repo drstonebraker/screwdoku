991c1eea775a9dd75e78dd9b750bdd7b90be1504
App Academy Student <student@appacademy.io>
2017-08-03 14:52:53 -0700

    Fought evil and won

--- sudoku.rb
+++ sudoku.rb
@@ -1,16 +1,16 @@
 require_relative "board"
 require 'colorize'
 
-puts "Only contractors write code this bad.".yellow
+# puts "Only contractors write code this bad.".yellow
 
 class SudokuGame
   def self.from_file(filename)
-    board = self.from_file(filename)
+    board = Board.from_file(filename)
     self.new(board)
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
   def method_missing(method_name, *args)
@@ -22,6 +22,10 @@ class SudokuGame
     end
   end
 
+  def parse_pos(string)
+    string.split(',').map(&:to_i)
+  end
+
   def get_pos
     pos = nil
     until pos && valid_pos?(pos)
@@ -31,7 +35,7 @@ class SudokuGame
       begin
         pos = parse_pos(gets)
       rescue
-        TODO: Google how to print the error that happened inside of a rescue statement.
+        # TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
@@ -39,6 +43,10 @@ class SudokuGame
       end
     end
     pos
+  end
+
+  def parse_val(string)
+    string.to_i
   end
 
   def get_val
@@ -55,7 +63,7 @@ class SudokuGame
     board.render
     pos = get_pos
     val = get_val
-    board[*pos] = val
+    board[pos] = val
   end
 
   def run
@@ -65,16 +73,17 @@ class SudokuGame
   end
 
   def solved?
-    self.solved?
+    board.solved?
   end
 
   def valid_pos?(pos)
-    if pos.is_a?(:Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |x| (0...board.size).include? x }
       return true
     else
       get_pos
+    end
   end
 
   def valid_val?(val)
@@ -88,3 +97,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
